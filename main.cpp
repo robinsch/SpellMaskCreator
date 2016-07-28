bca15b1e6e564a7ee9088184af01cbd539a145d3
robinsch <robin.schriever.hude@web.de>
2016-07-28 12:41:00 +0200

    print out correct values

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 #include <functional>
 #include "DBCStores.h"
 #include "SharedDefines.h"
+#include <inttypes.h>
 
 void CreateMask(std::function<bool(SpellEntry const*)> const& check)
 {
-    uint32_t resultMask = 0x0;
+    uint64_t resultMask = 0x0;
     for (uint32_t i = 1; i < sSpellStore.GetNumRows(); ++i)
     {
         SpellEntry const* spellInfo = sSpellStore.LookupEntry(i);
@@ -14,11 +15,11 @@ void CreateMask(std::function<bool(SpellEntry const*)> const& check)
         if (check(spellInfo))
         {
             resultMask |= spellInfo->SpellFamilyFlags;
-            printf("Name: %u Entry: %s SpellFamilyFlags: %u\n", spellInfo->Id, spellInfo->SpellName[0], spellInfo->SpellFamilyFlags);
+            printf("Name: %u Entry: %s SpellFamilyFlags: %" PRIu64" \n", spellInfo->Id, spellInfo->SpellName[0], spellInfo->SpellFamilyFlags);
         }
     }
 
-    printf("\nSpellFamilyMask: %u", resultMask);
+    printf("\nSpellFamilyMask: %" PRIu64, resultMask);
 }
 
 int main()
@@ -29,8 +30,8 @@ int main()
     CreateMask([](SpellEntry const* spellInfo)
     {
         // Example how to use
-        /*for (uint8_t i = 0; i < 3; i++)
-            return spellInfo->Dispel == DISPEL_MAGIC && spellInfo->SpellFamilyName == SPELLFAMILY_SHAMAN && spellInfo->Effect[i] == SPELL_EFFECT_APPLY_AURA;*/
+        for (uint8_t i = 0; i < 3; i++)
+            return spellInfo->SpellFamilyFlags && spellInfo->Dispel == DISPEL_MAGIC && spellInfo->SpellFamilyName == SPELLFAMILY_SHAMAN && spellInfo->Effect[i] == SPELL_EFFECT_APPLY_AURA;
 
         return false;
     });
